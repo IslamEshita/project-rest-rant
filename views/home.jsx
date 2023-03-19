d8dccaf13d7c476da26cfad3947d427718cbedf1
Eshita Islam <islam.eshita@gmail.com>
2023-03-19 14:46:09 -0700

    Adding a home image

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div className='homeImage'><img src="/images/fruit_sandwich.jpg" alt="Fruit Sandwich" /></div>
+              <div>
+              Photo by <a href="https://unsplash.com/@miracletwentyone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joseph Gonzalez</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
+              
+  
           </main>
       </Def>
     )
