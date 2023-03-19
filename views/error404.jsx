523039028ea10d9e87f83d19e61b83ee9584c4bb
Eshita Islam <islam.eshita@gmail.com>
2023-03-19 15:06:42 -0700

    Added styling for the CSS page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,9 @@ function error404() {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Ooops, sorry we can't find this page!</p>
+              <h2>Ooops, sorry we can't find this page!</h2>
+              <div className='pageNotFound'><img src="/images/page_not_found.jpg" alt="Page Not Found (Dog with glasses)" /></div>
+              <div>Photo by <a href="https://unsplash.com/@charlesdeluvio?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">charlesdeluvio</a> on <a href="https://unsplash.com/images/feelings/funny?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></div>                            
           </main>
       </Def>
     )
