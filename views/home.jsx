326ecb5dfb6deff993b5debf3a23478d55be7498
Eshita Islam <islam.eshita@gmail.com>
2023-03-20 18:08:02 -0700

    Added some styles to the home page

--- views/home.jsx
+++ views/home.jsx
@@ -5,13 +5,19 @@ function home() {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
-              <div className='homeImage'><img src="/images/fruit_sandwich.jpg" alt="Fruit Sandwich" /></div>
-              <div>
-              Photo by <a href="https://unsplash.com/@miracletwentyone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joseph Gonzalez</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              <h1>REST-Rant</h1>
+              <h2>A cool restaurant review app</h2>
+              <div className="container home">
+                <div className="row">
+                  <div className="col-md-8 mx-auto">
+                    <div className='homeImage'><img src="/images/fruit_sandwich.jpg" alt="Fruit Sandwich" /></div>
+                    <div>
+                    Photo by <a href="https://unsplash.com/@miracletwentyone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joseph Gonzalez</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                    <a href="/places/" className="restaurantLink text-right">Restaurants</a>
+                  </div>
+                </div>
               </div>
-              
-  
           </main>
       </Def>
     )
