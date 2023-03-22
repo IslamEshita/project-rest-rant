0230f99fda9cfac7df97ae05a9636bbc74fecd08
Eshita Islam <islam.eshita@gmail.com>
2023-03-22 07:50:12 -0700

    Added rating,comment and description

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,24 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
+          <main className='col-lg-8 showMain'>
+            <h1>{data.place.name}</h1>
+            <img src={data.place.pic} className='showImage'></img>
+            <section className='rating'>
+                <h2>Rating</h2>
+                <p>Not Rated</p>
+            </section>
+            <section className='description'>
+                <h2>Description</h2>
+                <p>Located in {data.place.city},{data.place.state} and serving {data.place.cuisines}</p>
+            </section>
+            <section className='comments'>
+                <h2>Comments</h2>
+                <p>No comments yet!</p>
+            </section>
           </main>
         </Def>
     )
